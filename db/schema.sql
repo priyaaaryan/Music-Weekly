e079291cd7406aab2d970beab10874cd6812e786
Prerana Shukla <shuklaprerana01@gmail.com>
2022-08-31 21:16:26 -0400

    update DB struct

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,25 @@
 DROP DATABASE musicweekly;
 CREATE DATABASE musicweekly;
 Use musicweekly;
+
 CREATE TABLE Users (
-    id int NOT NULL AUTO_INCREMENT,
-    Username varchar(255) NOT NULL,
-    Password varchar(255) NOT NULL,
-    PRIMARY KEY (id)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY(id)
 );
+
 CREATE TABLE POSTS (
     id int NOT NULL AUTO_INCREMENT,
     Title varchar(255) NOT NULL,
-    post_url varchar(255) NOT NULL,
+    Instuction varchar(255) NOT NULL,
+    created_at datetime not null DEFAULT CURRENT_TIMESTAMP,
+    Attached_type varchar(100),
     user_id int,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES Users(id)
 ); 
 
-CREATE TABLE `file` (
-  `id` int(11) NOT NULL,
-  `filepath` varchar(100) NOT NULL,
-  `filetype` int(11) NOT NULL,
-  `updated_by` varchar(255) NOT NULL,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  user_id int,
-  FOREIGN KEY (user_id) REFERENCES Users(id)
-);
 
 
