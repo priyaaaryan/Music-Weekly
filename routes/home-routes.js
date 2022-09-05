143c701dea01976e4d4699990709202fb235c339
Tara Evans <tarajevans@hotmail.com>
2022-09-05 12:23:14 -0400

    created home-routes

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -2,6 +2,9 @@ const router = require("express").Router();
 const homeController = require("../controllers/home-controller");
 
 router.get("/", homeController);
+router.get("/blog", homeController);
+router.get("/musician", homeController);
+router.get("/classroom", homeController);
 router.get("*", homeController);
 
 module.exports = router;
