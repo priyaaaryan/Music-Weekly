ede8f69d3cac25ad0d08920ae9e1f332c78ff609
Tara Evans <tarajevans@hotmail.com>
2022-09-05 12:21:16 -0400

    created home controller

--- controllers/home-controller.js
+++ controllers/home-controller.js
@@ -5,4 +5,16 @@ router.get("/",(req,res)=>{
   res.render("homepage");
 })
 
+router.get("/blog",(req,res)=>{
+  res.render("blog");
+})
+
+router.get("/musicians",(req,res)=>{
+  res.render("musicians");
+})
+
+router.get("/classroom",(req,res)=>{
+  res.render("classroom");
+})
+
 module.exports = router;
