d93505d02eae75d1a0f7d44bae9ca598df12f6c7
priyaaaryan <106102991+priyaaaryan@users.noreply.github.com>
2022-09-14 00:25:23 -0400

    fixed port setting for heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const express = require("express");
 
 // Initialize (create) the express object.
 const app = express();
-const PORT = process.env.SERVER_PORT || 8080;
+const PORT = process.env.PORT || 3001;
 const sess = {
   secret: "Super secret secret",
   cookie: {},
